5570dc999cd216a8b4537a7a9cbb04e71df3ca57
Vitaly Bastrikov <82602126+vitaly-bastrikov@users.noreply.github.com>
2022-03-25 18:06:10 -0400

    initial commit

---
+++ src/main/java/io/bastrikov/ml/MlApplication.java
@@ -0,0 +1,13 @@
+package io.bastrikov.ml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/bastrikov/ml/MlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bastrikov.ml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
